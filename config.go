e91574b49c529caca19576385588e0840bb4043e
dingdinglz <1149978856@qq.com>
2024-11-28 15:52:06 +0800

    ✨ feat(config): 保存和加载

--- config.go
+++ config.go
@@ -1,5 +1,10 @@
 package dingadmin
 
+import (
+	"encoding/json"
+	"os"
+)
+
 type Config struct {
 	Port       int
 	Prefix     string
@@ -18,4 +23,27 @@ var DefaultConfig Config = Config{
 	Author:     "dinglz",
 	AuthorLink: "https://github.com/dingdinglz",
 	Theme:      "./web/admin/",
+}
+
+// SaveConfig 保存Config
+// p 为保存的文件路径，例如a.json
+func (app *App) SaveConfig(p string) {
+	c, _ := json.Marshal(&app.config)
+	os.WriteFile(p, c, os.ModePerm)
+}
+
+// LoadConfig 加载Config
+// p 要加载的文件路径
+// 返回一个Config
+func LoadConfig(p string) (Config, error) {
+	b, e := os.ReadFile(p)
+	if e != nil {
+		return DefaultConfig, e
+	}
+	var i Config
+	e = json.Unmarshal(b, &i)
+	if e != nil {
+		return DefaultConfig, e
+	}
+	return i, nil
 }
--- ding_test.go
+++ ding_test.go
@@ -3,9 +3,8 @@ package dingadmin
 import "testing"
 
 func TestDingAdmin(t *testing.T) {
-	cfg := DefaultConfig
-	cfg.Theme = "./dingadmin-layui/"
-	app := New(cfg)
+	c, _ := LoadConfig("test.json")
+	app := New(c)
 	app.AddMenu(Menu{Name: "test", Text: "测试菜单", Href: "/test"})
 	app.AddMenuGroup(MenuGroup{Text: "测试菜单组", Menus: []Menu{{Name: "test2", Text: "测试菜单2", Href: "/test2"}}})
 	app.Serve()
